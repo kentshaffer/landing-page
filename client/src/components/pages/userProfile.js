e85a3a5c79953c5a93756311a918f42cf07c83d5
kent shaffer <kentshaffer@kents-MacBook-Pro.local>
2023-02-03 00:27:42 -0800

    started ui layout for homepage

--- client/src/components/pages/userProfile.js
+++ client/src/components/pages/userProfile.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import profilePhoto from '../images/profPic.png'
 
 
 export default function Profile() {
   return (
     <div>
       <h1>profile</h1>
+      <div>
+        header image
+        <div>profile image</div>
+      </div>
+      <div>about me</div>
+      <h2>Your Posts</h2>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={profilePhoto} />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
