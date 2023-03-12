865e37338bfe8b897a46711ec9892712a769aac7
kent shaffer <kentshaffer@kents-MacBook-Pro.local>
2023-03-12 00:27:39 -0800

    deleted old post card, as new one works and the style is better. started to arrange profile layout

--- client/src/components/pages/userProfile.js
+++ client/src/components/pages/userProfile.js
@@ -1,31 +1,21 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
 import profilePhoto from '../images/profPic.png'
+import backgroundPhoto from '../images/landscapePlaceholder.png'
 import '../styles/style.css'
 
 
 export default function Profile() {
   return (
     <div>
-      <h1>profile</h1>
       <div>
-        header image
-        <div>profile image</div>
+        <h1>profile</h1>
+        <img className='backgroundPhoto' src={backgroundPhoto} alt='placeholder'></img>
+        <img className='profilePhoto' src={profilePhoto} alt='placeholder'></img>
+        <div>about me</div>
       </div>
-      <div>about me</div>
       <h2>Your Posts</h2>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={profilePhoto} />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
       <div className='post-card-div'>
         <div className='post-title-container'>
           <img className='profilePhoto' src={profilePhoto} alt='placeholder'></img>
