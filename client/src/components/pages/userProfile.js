f4376f69ef528d5f9ecd8f70577c06526443a2bb
kent shaffer <kentshaffer@kents-MacBook-Pro.local>
2023-03-14 00:52:34 -0700

    header photo styling and layout

--- client/src/components/pages/userProfile.js
+++ client/src/components/pages/userProfile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import Button from 'react-bootstrap/Button';
 // import Card from 'react-bootstrap/Card';
+import avatar from '../images/profPic.png'
 import profilePhoto from '../images/profPic.png'
 import backgroundPhoto from '../images/landscapePlaceholder.png'
 import '../styles/style.css'
@@ -9,16 +10,18 @@ import '../styles/style.css'
 export default function Profile() {
   return (
     <div>
-      <div>
-        <h1>profile</h1>
+      <div className='profileHeader'>
         <img className='backgroundPhoto' src={backgroundPhoto} alt='placeholder'></img>
         <img className='profilePhoto' src={profilePhoto} alt='placeholder'></img>
-        <div>about me</div>
+      </div>
+      <div>
+        <h2>Foo Bar</h2>
+        <p>San Diego * out here for vibes * love to cook</p>
       </div>
       <h2>Your Posts</h2>
       <div className='post-card-div'>
         <div className='post-title-container'>
-          <img className='profilePhoto' src={profilePhoto} alt='placeholder'></img>
+          <img className='avatar' src={avatar} alt='placeholder'></img>
           <div className='userName'>Foo Bar</div>
           <div className='postDate'>01/01/0101 01:01</div>
         </div>
